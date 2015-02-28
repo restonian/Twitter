f956a4fbcc3dc81b1e50cdd1740064a9ce7aaac5
Robert Saffern <restonian@hotmail.com>
2015-02-28 12:33:08 -0500

    This is a test of using RStudio with GitHub

---
+++ Streaming Tweets.R
@@ -0,0 +1,21 @@
+#From the Pablo Barbera PDF Scaping Twitter and Web Data Using R
+
+#Chunk 1 Load packages and register cert
+PrepareTwitter()
+load("twitter authentication.Rdata")
+registerTwitterOAuth(Cred)
+##################################################################
+
+#Chunk 2 get tweets, save file and parse 
+filterStream(file.name = "tweets_keyword.json", track = c("obama", "biden"),
+             timeout = 180, oauth = Cred)
+tweets <- parseTweets("tweets_keyword.json", verbose = TRUE)
+
+#chunk 3 clean tweets
+
+clean.tweets <- function(text){
+  words <- removePunctuation(text)
+  words <- wordStem(words)
+  words <- str_split(text, " ")
+  return(words)
+}
